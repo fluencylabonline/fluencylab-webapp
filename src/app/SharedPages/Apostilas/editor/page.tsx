5e1efd10d18f29c5dd3d0db0ef11ca97ac1128f1
fluencylabonline <fluencylab.online@gmail.com>
2024-06-21 10:35:23 -0300

    fixed some bugs 3

--- src/app/SharedPages/Apostilas/editor/page.tsx
+++ src/app/SharedPages/Apostilas/editor/page.tsx
@@ -1,27 +1,27 @@
 'use client'
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-//Firebase
+// Firebase
 import { getDoc, doc, setDoc } from 'firebase/firestore';
 import { db } from '@/app/firebase';
 
-//TipTap
-import Tiptap from './TipTap'
+// TipTap
+import Tiptap from './TipTap';
 import DocumentAnimation from '@/app/ui/Animations/DocumentAnimation';
 
 const NotebookEditor = () => {
+  const router = useRouter();
+  const { query } = router;
+  const { workbook, lesson } = query;
+
   const [content, setContent] = useState<string>('');
   const [loading, setLoading] = useState(true);
   const [isTyping, setIsTyping] = useState(false);
   let typingTimeout: ReturnType<typeof setTimeout> | null = null;
-  const params = new URLSearchParams(window.location.search);
-  const workbook = params.get('workbook');
-  const lesson = params.get('lesson');
-  useEffect(() => {
-    // Fetching data only on the client-side
-    const fetchNotebookContent = async () => {
-      
 
+  useEffect(() => {
+    const fetchNotebookContent = async () => {
       try {
         setLoading(true); // Set loading to true when fetching content
         const notebookDoc = await getDoc(doc(db, `Notebooks/${workbook}/Lessons/${lesson}`));
@@ -34,11 +34,11 @@ const NotebookEditor = () => {
         setLoading(false);
       }
     };
-
-    if (typeof window !== 'undefined') {
+    
+    if (workbook && lesson) {
       fetchNotebookContent();
     }
-  }, []); // Empty dependency array ensures this runs once after the initial render
+  }, [workbook, lesson]);
 
   const handleContentChange = async (newContent: string) => {
     if (!isTyping) {
@@ -54,9 +54,7 @@ const NotebookEditor = () => {
     }, 3000);
 
     try {
-      const params = new URLSearchParams(window.location.search);
-      const lesson = params.get('lesson');
-      if (lesson) {
+      if (workbook && lesson) {
         await setDoc(doc(db, `Notebooks/${workbook}/Lessons/${lesson}`), { content: newContent }, { merge: true });
       }
     } catch (error) {
@@ -76,7 +74,7 @@ const NotebookEditor = () => {
         isTyping={isTyping}
       />
     </div>
-  )
-}
+  );
+};
 
 export default NotebookEditor;
