f64173c199bf1212612a2f9e5884887b1e262532
fluencylabonline <fluencylab.online@gmail.com>
2024-07-22 20:52:17 -0300

    fixes

--- src/app/teacher-dashboard/suporte/curso/dinamica-de-aulas/dinamica2.tsx
+++ src/app/teacher-dashboard/suporte/curso/dinamica-de-aulas/dinamica2.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Dinamica2(){
     return(
         <div className="flex flex-col items-center">
-            <div className='mx-2 flex flex-col items-start justify-center w-[80vw]'>
+            <div className='mx-2 flex flex-col items-start justify-center w-full'>
                 <p className='text-2xl font-bold py-2'>O que levar em conta nas aulas</p>
                 <p>É interessante que cada aula (e no seu planejamento também) tenha materiais, métodos e ferramentas que o aluno goste e aproveite mais. Como músicas, trechos de séries ou filmes, livros, jogos ou dinânicas e assim por diante.</p>
                 <p>Sempre dê um feedback contínuo para seus alunos, mostre onde ele já melhorou e como ele melhorou. Esses elogios vão ser muito importantes para que ele se sinta motivado e continue progredindo. O ajude a perceber o que ele ainda precisa melhorar. Mantenha um equilíbrio entre a cobrança e os elogios para que o aluno possa continuar motivado. </p>
@@ -14,7 +14,7 @@ export default function Dinamica2(){
                 <p>Tente criar um vínculo com seus alunos, pergunte sobre a semana ou o dia. Sobre o que gostam e o que acham interessante, isso pode motivar o aluno a estudar mais e ficar mais animado para cada aula. </p>
             </div>
         
-            <div className='w-[85vw] lg:flex lg:flex-row-reverse md:flex md:flex-col flex flex-col gap-1 justify-between items-start my-4 p-4 rounded-md bg-fluency-gray-200 dark:bg-fluency-gray-900'>
+            <div className='w-full lg:flex lg:flex-row-reverse md:flex md:flex-col flex flex-col gap-1 justify-between items-start my-4 p-4 rounded-md bg-fluency-gray-200 dark:bg-fluency-gray-900'>
                 <p className='w-fit p-6'>Preparamos um resumo em vídeo para te ajudar com esse assunto. Também é um vídeo curto, não se preocupa!</p>
                 <iframe className='w-full rounded-md' width="720" height="275" src="https://www.youtube.com/embed/S6wYV6i-5rs" title="Nivelamento" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
