1f41b3fae5d169bad4a236620be485c5cdf623f7
fluencylabonline <fluencylab.online@gmail.com>
2024-08-01 20:12:19 -0300

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 //Pages Import
-import Header from '@/app/Landing/Header/page'
-import About from '@/app/Landing/About/page';
 import Team from '@/app/Landing/Team/page';
 import Questions from '@/app/Landing/Questions/page';
 import Footer from '@/app/Landing/Footer/page';
@@ -12,13 +10,6 @@ export default function Home() {
 
   return (
     <div className='bg-fluency-bg-light dark:bg-fluency-bg-dark pt-1'>
-    
-    {/* <Header />
-        <About />
-        <Team />
-        <Questions />
-        <Footer /> */}
-
         <NewHeader />
         <NewAbout />
         <Team />
