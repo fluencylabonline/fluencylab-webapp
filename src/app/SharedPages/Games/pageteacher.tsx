dbbabd0eb3819d196c59e06c79fafc22b07c858f
fluencylabonline <fluencylab.online@gmail.com>
2024-08-02 23:25:17 -0300

    admin

--- src/app/SharedPages/Games/pageteacher.tsx
+++ src/app/SharedPages/Games/pageteacher.tsx
@@ -41,6 +41,13 @@ export default function GamesProTeacher(){
                 <p className="flex flex-row gap-2 items-center justify-center"><span className="text-xl font-bold">TicTacToe</span></p>
             </div>
             </Link>
+
+            <Link href={"pratica/what-am-i"}>
+            <div className="w-auto h-[14.5rem] rounded-md p-5 bg-fluency-blue-100 dark:bg-fluency-gray-700 hover:bg-fluency-blue-200 hover:dark:bg-fluency-gray-800 flex flex-col gap-2 items-center justify-between cursor-pointer duration-200 ease-in-out transition-all">
+                <Image alt="Wordle" className="w-36 h-auto mt-4" src={WhatAmIImage} />
+                <p className="flex flex-row gap-2 items-center justify-center"><span className="text-xl font-bold">What Am I?</span></p>
+            </div>
+            </Link>
         </div>
     )
 }
