ab1198498931b386ce35f819ff45ebb6e33eeb67
fluencylabonline <fluencylab.online@gmail.com>
2024-08-08 20:28:39 -0300

    items

--- src/app/Landing/Header/page.tsx
+++ src/app/Landing/Header/page.tsx
@@ -224,13 +224,13 @@ const navigation = [
                 </p>
 
                 {isMobile ? (
-                <div className="container md:flex md:gap-2 md:flex-row md:content-center flex flex-rows gap-2 items-center mb-3">
-                    <button>
+                <div className="z-50 container md:flex md:gap-2 md:flex-row md:content-center flex flex-rows gap-2 items-center mb-3">
                     <Link href="/u/googlecalendarpage" className="w-max cursor-pointer gap-1 leading-6 inline-flex items-center px-4 py-2 bg-fluency-blue-500 hover:bg-fluency-blue-500 ease-in-out duration-300 text-fluency-text-dark text-sm font-medium rounded-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" /></svg>
-                        Agendar, ou
+                        <button>
+                            Agendar, ou
+                        </button>
                     </Link>
-                    </button>
-                    <WhatsAppButton buttonText="Mensagem aqui"/>
+                        <WhatsAppButton buttonText="Mensagem aqui"/>
                 </div>
                 
                 ) : (
