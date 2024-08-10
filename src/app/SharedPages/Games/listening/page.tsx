919de9171e2503f3d599b1501428c86a146755c3
fluencylabonline <fluencylab.online@gmail.com>
2024-08-10 11:27:53 -0300

    listening

--- src/app/SharedPages/Games/listening/page.tsx
+++ src/app/SharedPages/Games/listening/page.tsx
@@ -508,7 +508,7 @@ export default function Listening() {
                                 >
                                     {doc.name}
                                 </button>
-                                {session?.user.role === 'teacher' || 'admin' &&
+                                {session?.user.role === 'teacher' &&
                                <div className='flex flex-row gap-1 items-center'>
                                      <Tooltip className='px-1 bg-fluency-red-500 text-white font-bold rounded-md' content="Apagar áudio">
                                         <button
